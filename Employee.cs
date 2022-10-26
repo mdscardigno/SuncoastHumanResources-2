692671cbef15618aab8c440ea08c944c0c8ba801
Milagro Scardigno <mdscardigno@gmail.com>
2022-10-26 13:29:01 -0400

    Employee Class Attributes

---
+++ Employee.cs
@@ -0,0 +1,14 @@
+namespace SuncoastHumanResources
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Department { get; set; }
+        public int Salary { get; set; }
+        public int MonthlySalary()
+        {
+            return Salary / 12;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace SuncoastHumanResources
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Suncoast Human Resources");
+
+
+        }
     }
-  }
 }
