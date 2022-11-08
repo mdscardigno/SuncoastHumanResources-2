f0c59303b100613d2289fa0ba41638f5e637a8c0
Milagro Scardigno <mdscardigno@gmail.com>
2022-11-07 20:20:51 -0500

    EmployeeDB

--- EmployeeDB.cs
+++ EmployeeDB.cs
@@ -1,7 +1,13 @@
 namespace SuncoastHumanResources
 {
-    public class EmployeeDB
+    class EmployeeDB
     {
-
+        //API
+        //Move our List<Employee> inside.
+        //Make this class property private.
+        //Develop public methods to perform common actions we need:
+        //GetAllEmployees. FindOneEmployee. AddEmployee. UpdateEmployee. DeleteEmployee.
+        //Methods receive data it needs and returns the data it provides.
+        //These methods essentially create an API or Application Programming Interface for how to use this code.
     }
 }
