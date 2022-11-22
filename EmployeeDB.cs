760faa2a73a33872423b402aa4bdea3cf1e9f1c1
Milagro Scardigno <mdscardigno@gmail.com>
2022-11-21 19:43:46 -0500

    adds code to load the employees

--- EmployeeDB.cs
+++ EmployeeDB.cs
@@ -69,6 +69,7 @@ namespace SuncoastHumanResources
         {
             Employee foundEmployee = FindOneEmployee(employeeToDelete);
             Employees.Remove(foundEmployee);
+
         }
 
         //UPDATE
