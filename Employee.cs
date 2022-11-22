46d26d12a67f8666cbdc47a92e5c98b886f3d641
Milagro Scardigno <mdscardigno@gmail.com>
2022-11-21 19:15:09 -0500

    can write out employees

--- Employee.cs
+++ Employee.cs
@@ -5,6 +5,9 @@ namespace SuncoastHumanResources
         public string Name { get; set; }
         public int Department { get; set; }
         public int Salary { get; set; }
+        //it is not going to write out any of the behaviors because the behavior is shared amongst all employees. It is just going to write out the properties.
+        //the only thing specific to an employee is the name, department and salary.
+        //behavior is shared and state is individual to instances of employees in our list or instances of them in our file.
         public int MonthlySalary()
         {
             return Salary / 12;
