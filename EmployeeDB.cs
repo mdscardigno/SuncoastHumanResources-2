26bfd748a6e0f1f3900634578dae08440587ab44
Milagro Scardigno <mdscardigno@gmail.com>
2022-11-21 19:19:29 -0500

    notes about reader to load employees

--- EmployeeDB.cs
+++ EmployeeDB.cs
@@ -17,55 +17,64 @@ namespace SuncoastHumanResources
         //Method to load Employees (does not return anything. Just populates Employees lists.) 
         public void LoadEmployees()
         {
+            if (File.Exists("employees.csv"))
+            {
+                var fileReader = new StreamReader("employees.csv");
+                var csvReader = new CsvReader(fileReader, CultureInfo.InvariantCulture);
+                Employees = csvReader.GetRecords<Employee>().ToList();
+                //in this case we do not have to configure the csvReader to ignore the header because I want the headers 
+                //there because we are going to use that header to know in what order to read the various properties.
 
-
-
-        }
-        //Write the list Employee to a file.
-        public void SaveEmployees()
-        {
-            var fileWriter = new StreamWriter("employees.csv");
-            var csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture);
-            csvWriter.WriteRecords(Employees);//csv is smart enough to know that we are writing a list of employees objects
-            fileWriter.Close();
-        }
-        //Develop public methods to perform common actions we need:
-        //GetAllEmployees. FindOneEmployee. AddEmployee. UpdateEmployee. DeleteEmployee.
-        //CREATE Add Employee
-        //BEHAVIOR
-        public void AddEmployee(Employee newEmployee)
-        {
-            Employees.Add(newEmployee);
-        }
-        //READ
-        //BEHAVIOR
-        public List<Employee> GetAllEmployees()
-        {
-            return Employees;
+            }
         }
 
-        //READ
-        //We created this API that says: If you send me a name, and you call FindOneEmployee, I will FindOneEmployee and return it. 
-        //separating code based on area of concern is decoupling.
-        //We don't want interface code, searching code, prompting code, list code and data code all in one place.
-        public Employee FindOneEmployee(string nameToFind)
-        {
-            //In one place I declared how I find employees. 
-            Employee foundEmployee = Employees.FirstOrDefault(employee => employee.Name.ToUpper().Contains(nameToFind.ToUpper()));
-            return foundEmployee;
-        }
 
-        //DELETE
-        public void DeleteEmployee(string employeeToDelete)
-        {
-            Employee foundEmployee = FindOneEmployee(employeeToDelete);
-            Employees.Remove(foundEmployee);
-        }
-
-        //UPDATE
-
-
-        //Methods receive data it needs and returns the data it provides.
-        //These methods essentially create an API or Application Programming Interface for how to use this code.
     }
+    //Write the list Employee to a file.
+    public void SaveEmployees()
+    {
+        var fileWriter = new StreamWriter("employees.csv");
+        var csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture);
+        csvWriter.WriteRecords(Employees);//csv is smart enough to know that we are writing a list of employees objects
+        fileWriter.Close();
+    }
+    //Develop public methods to perform common actions we need:
+    //GetAllEmployees. FindOneEmployee. AddEmployee. UpdateEmployee. DeleteEmployee.
+    //CREATE Add Employee
+    //BEHAVIOR
+    public void AddEmployee(Employee newEmployee)
+    {
+        Employees.Add(newEmployee);
+    }
+    //READ
+    //BEHAVIOR
+    public List<Employee> GetAllEmployees()
+    {
+        return Employees;
+    }
+
+    //READ
+    //We created this API that says: If you send me a name, and you call FindOneEmployee, I will FindOneEmployee and return it. 
+    //separating code based on area of concern is decoupling.
+    //We don't want interface code, searching code, prompting code, list code and data code all in one place.
+    public Employee FindOneEmployee(string nameToFind)
+    {
+        //In one place I declared how I find employees. 
+        Employee foundEmployee = Employees.FirstOrDefault(employee => employee.Name.ToUpper().Contains(nameToFind.ToUpper()));
+        return foundEmployee;
+    }
+
+    //DELETE
+    public void DeleteEmployee(string employeeToDelete)
+    {
+        Employee foundEmployee = FindOneEmployee(employeeToDelete);
+        Employees.Remove(foundEmployee);
+    }
+
+    //UPDATE
+
+
+    //Methods receive data it needs and returns the data it provides.
+    //These methods essentially create an API or Application Programming Interface for how to use this code.
+}
 }
